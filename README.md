f630c7293d5cb1c78c57cbd3b326aa5d32f6adcc
Charlotte.Hong <hunandy14@gmail.com>
2022-01-15 01:40:46 +0900

    update 新增復原

--- README.md
+++ README.md
@@ -3,4 +3,7 @@
 
 使用方法
 1. 按下 Win+X 再按 A 打開終端機
-2. 輸入 `irm bit.ly/3fqFUMs|iex` 然後按下 Enter 即可
+2. 輸入 `irm bit.ly/3fqFUMs|iex; DisableVideoDriverUpdate` 然後按下 Enter 即可
+
+復原方法
+1. 輸入 `irm bit.ly/3fqFUMs|iex; DisableVideoDriverUpdate -Recovery` 然後按下 Enter 即可
--- VideoDriverUpdate.ps1
+++ VideoDriverUpdate.ps1
@@ -1,12 +1,20 @@
-function VideoDriverUpdate {
-    $Device = ((Get-WmiObject -Class CIM_PCVideoController)|Select-Object Name, PNPDeviceID) -match("NVIDIA|AMD")
+function DisableVideoDriverUpdate {
+    param(
+        [switch] $Recovery
+    )
+    $Device = ((Get-WmiObject -Class CIM_PCVideoController)|Select-Object Name, PNPDeviceID)# -match("NVIDIA|AMD")
     if ($Device -eq "") { Write-Host "ERROR:: No (NVIDIA or AMD) Device" -F:yellow; return }
     $DeviceID = "PCI\"+($Device.PNPDeviceID.Split('\'))[1]
-
-    $regPath = "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions"
-    reg add $regPath /f /t "REG_DWORD" /v "DenyDeviceIDs" /d "1"
-    reg add $regPath /f /t "REG_DWORD" /v "DenyDeviceIDsRetroactive" /d "0"
-    $regPath = "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyDeviceIDs"
-    reg add $regPath /f /t "REG_SZ" /v "1" /d $DeviceID
+    
+    $regPath1 = "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions"
+    $regPath2 = "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DeviceInstall\Restrictions\DenyDeviceIDs"
+    
+    if ($Recovery) {
+        reg delete $regPath2
+    } else {
+        reg add $regPath1 /f /t "REG_DWORD" /v "DenyDeviceIDs" /d "1"
+        reg add $regPath1 /f /t "REG_DWORD" /v "DenyDeviceIDsRetroactive" /d "0"
+        reg add $regPath2 /f /t "REG_SZ" /v "1" /d $DeviceID
+    }
 }
-VideoDriverUpdate
+# DisableVideoDriverUpdate
